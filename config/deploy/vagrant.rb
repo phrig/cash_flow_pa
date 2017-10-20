14bdbb88e24b48ce39c0bf2ed48c95182fc2d902
pca2 <atwaterc@gmail.com>
2017-10-19 21:47:03 -0400

    Vagrant -- add init vagrant cap deploy file

---
+++ config/deploy/vagrant.rb
@@ -0,0 +1,26 @@
+# Capistrano 3.x Vagrant stage
+# config/deploy/vagrant.rb
+
+set :stage, :vagrant
+set :rails_env, "production"
+
+vagrant_ssh_config = `vagrant ssh-config`.split("\n")[1..-1].map(&:strip).inject({}) do |m, s|
+  k, v = s.split(/\s/, 2).map(&:strip); m[k] = v; m
+end
+
+server vagrant_ssh_config['HostName'],
+  roles: %w{web app db},
+  primary: true,
+  user: vagrant_ssh_config['User'],
+  port: vagrant_ssh_config['Port'],
+  ssh_options: {
+    keys: [vagrant_ssh_config['IdentityFile']],
+    forward_agent: vagrant_ssh_config['ForwardAgent'] == 'yes',
+    keys_only: 'yes'
+
+  }
+set :deploy_to, '/rails/cash_flow_pa'
+set :default_env, {
+  path: "/home/ubuntu/.rvm/gems/ruby-2.3.4/bin:$PATH"
+}
+set :branch, ENV["branch"] || "master"
